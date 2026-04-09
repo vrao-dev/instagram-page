3f161dbb4b89705b5ecc50272d182038d549eb17
Venky <venkateswararaoa118@gmail.com>
2026-04-09 14:45:03 +0530

    modified facebook login pages

--- js/basic.js
+++ js/basic.js
@@ -2,23 +2,17 @@ function showpassword(){
    let image='';
 
    const img = document.getElementById("show-img");
-   
-  
-
    const input = document.getElementById("password");
+
    if(input.type==="password"){
       input.type="text";
-      image='show';
-      
+      image='show';      
    }
    else{
       input.type="password";
       image='hide';
    }
-   
-
    img.src=`images/${image}.png`;
-
 }
 
 
@@ -29,5 +23,7 @@ function keydown(event){
    if (event.key==='Enter'){
       incorrect();
       accountPage();
+      colorchange();
+
    }
 }
